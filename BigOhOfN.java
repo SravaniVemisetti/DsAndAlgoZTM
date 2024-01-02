69ee2d0f3182b772ae4d4b0c9bcb739e805a006c
SravaniVemisetti <86417920+SravaniVemisetti@users.noreply.github.com>
2024-01-02 18:09:57 +0530

    Update BigOhOfN.java

--- BigOhOfN.java
+++ BigOhOfN.java
@@ -4,10 +4,13 @@ package com.ztm.ds.algo;
 public class BigOhOfN {
 
 	public static void main(String[] args) {
-		int[] arr = new int[5];
+		long t1 = System.currentTimeMillis();
+		int[] arr = new int[100000];
 		for (int i = 0; i < arr.length; i++) { //o(n) - linear - no of operations increases as no of elements increases.
 			System.out.println(i);
 		}
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2-t1);
 	}
 
 }
