af4c6ceb9f1a65ffd02687c412b90fb4aa57cd54
SravaniVemisetti <sravai.vemisetti@gmail.com>
2024-09-08 13:16:00 +0530

    test

--- BigOhOfN.java
+++ BigOhOfN.java
@@ -7,7 +7,7 @@ public class BigOhOfN {
 		long t1 = System.currentTimeMillis();
 		int[] arr = new int[100000];
 		for (int i = 0; i < arr.length; i++) { //o(n) - linear - no of operations increases as no of elements increases.
-			System.out.println(i);
+//			System.out.println(i);
 		}
 		long t2 = System.currentTimeMillis();
 		System.out.println(t2-t1);
